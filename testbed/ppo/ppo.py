af0d9e6b0584ba24925f07aa72454249008950b0
Naoto Yoshida <yossy0157@gmail.com>
2021-02-02 16:42:01 +0900

    add value function optimizer. comment out bootstrap

--- testbed/ppo/ppo.py
+++ testbed/ppo/ppo.py
@@ -6,6 +6,7 @@ from attr import attrs, attrib
 
 import torch
 from torch import Tensor
+import torch.optim as optim
 
 from testbed.ppo.network import ValueNetwork, PolicyNetwork
 
@@ -25,6 +26,9 @@ class PPOAgent:
                  max_time_steps,
                  dim_observation=2,
                  dim_action=2,
+                 lr_value=0.001,
+                 iter_op_vf=20,
+                 lr_policy=0.001,
                  ):
         """
 
@@ -40,16 +44,36 @@ class PPOAgent:
             hidden_size1=20,
             hidden_size2=20
         )
+
         self._policy_network = PolicyNetwork(
             dim_observation=dim_observation,
             hidden_size1=20,
             hidden_size2=20,
             dim_action=dim_action
         )
+        self._policy_old = PolicyNetwork(
+            dim_observation=dim_observation,
+            hidden_size1=20,
+            hidden_size2=20,
+            dim_action=dim_action
+        )
+        # copy policy into the old-policy network
+        self._policy_old.load_state_dict(state_dict=self._policy_network.state_dict())
+
+        # Optimizer
+        self._optimizer_vf = optim.Adam(
+            params=self._value_network.parameters(),
+            lr=lr_value,
+        )
+        self._optimizer_policy = optim.Adam(
+            params=self._policy_network.parameters(),
+            lr=lr_policy,
+        )
 
         # PPO params
         self._n_trajectory = n_trajectory
         self._max_time_steps = max_time_steps
+        self._iteration_op_value = iter_op_vf
 
         # Internal state of the system
         self._trajectory = deque()
@@ -74,7 +98,19 @@ class PPOAgent:
                                    is_done)
         return action
 
-    def _add_data_into_buffer(self, observation, reward, action, is_done):
+    def _add_data_into_buffer(self,
+                              observation: List,
+                              reward: float,
+                              action: List,
+                              is_done: bool):
+        """
+        Trajectory Experience Buffer. The length of trajectories are shorter than than max_time_steps
+        :param observation:
+        :param reward:
+        :param action:
+        :param is_done:
+        :return:
+        """
         self._trajectory.append(Experience(observation, reward, action, is_done))
         if len(self._trajectory) == self._max_time_steps or is_done:
             self._data_buffer.append(self._trajectory)
@@ -87,17 +123,19 @@ class PPOAgent:
         reward_to_go = torch.zeros((self._n_trajectory, self._max_time_steps))
         advantage = torch.zeros_like(reward_to_go)
         for n, traj in enumerate(self._data_buffer):
+            # bootstrap
+            #obs_final = Tensor(traj[-1].observation)
+            #value_final = self._evaluate_vf(obs_final).detach()
             for t, _ in enumerate(traj):
                 sum_rew = 0
                 discount = 1.0
                 for experience in list(traj)[(t+1):]:
+                    # TODO: apply Generalized advantage estimation (GAE)
                     sum_rew += discount * experience.reward
                     discount *= self._reward_discount
 
                 # bootstrap
-                obs_final = Tensor(traj[-1].observation)
-                value_t = self._evaluate_vf(obs_final).detach()
-                sum_rew += discount * value_t
+                #sum_rew += discount * value_final
 
                 # reward to go
                 reward_to_go[n, t] = sum_rew
@@ -109,8 +147,39 @@ class PPOAgent:
 
         return advantage, reward_to_go
 
-    def update_vf(self):
-        pass
+    def _update_vf(self):
+        # TODO: update to efficient tensor computation
+        # TODO: summarize with advantage calculation
+        for epoch in range(self._iteration_op_value):
+            sum_error = torch.zeros(1)
+            len_data = 0.
+            for n, traj in enumerate(self._data_buffer):
+                len_data += len(traj)
+                # for bootstrap
+                #obs_final = Tensor(traj[-1].observation)
+                #value_final = self._evaluate_vf(obs_final).detach()
+                for t, _ in enumerate(traj):
+                    sum_rew = 0.
+                    discount = 1.0
+                    for experience in list(traj)[(t+1):]:
+                        # TODO: apply Generalized advantage estimation (GAE)
+                        sum_rew += discount * experience.reward
+                        discount *= self._reward_discount
+
+                    # bootstrap
+                    #sum_rew += discount * value_final
+
+                    # error
+                    obs_t = Tensor(traj[t].observation)
+                    value_t = self._evaluate_vf(obs_t)
+                    sum_error += (sum_rew - value_t)**2  # td error minimization?
+
+            # TODO: add gradient update
+            sum_error /= len_data
+            # print(f"VF minimization {epoch + 1}/{self._iteration_op_value}: {sum_error.detach().numpy()}")
+            self._optimizer_vf.zero_grad()
+            sum_error.backward()
+            self._optimizer_vf.step()
 
     def _evaluate_vf(self, observation: Tensor) -> Tensor:
         return self._value_network.forward(observation)
--- tests/test_ppo.py
+++ tests/test_ppo.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 import torch
 from pytest import approx
 
@@ -78,7 +80,7 @@ def test_get_advantage():
             assert approx((9 - t + value_t - agent._evaluate_vf(obs)).tolist(), advantage[n, t].tolist())
 
 
-def test_step():
+def test_agent_step():
     agent = PPOAgent(reward_discount=1.0,
                      n_trajectory=3,
                      max_time_steps=10,
@@ -91,3 +93,37 @@ def test_step():
     )
 
     assert action.size()[0] == 2
+
+
+def test_vf_update():
+    agent = PPOAgent(reward_discount=1.0,
+                     n_trajectory=3,
+                     max_time_steps=10,
+                     dim_observation=2,
+                     lr_value=0.1,
+                     iter_op_vf=50)
+
+    # Data for value estimation
+    # Value of all sequence should be one.
+    for i in range(3):
+        for t in range(10):
+            r = 1 if t == 9 else 0
+            is_done = True if t == 9 else False
+            agent.step(
+                observation=[t/9., 1-(t/9.)],
+                reward=r,
+                is_done=is_done,
+            )
+
+    agent._update_vf()
+
+    for i in range(3):
+        for t in range(10):
+            val = agent._evaluate_vf(
+                observation=Tensor([t/9., 1-(t/9.)])
+            )
+            if t == 9:
+                assert approx(0, val, abs=0.1)
+            else:
+                assert approx(1, val, abs=0.1)
+
